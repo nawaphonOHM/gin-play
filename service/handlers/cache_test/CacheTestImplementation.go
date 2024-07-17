829cd4341097b75a6986aa8f8ab19434d93e275d
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-07-17 14:49:08 +0700

    Implement Get method in CacheTestHandler
    The update adds functionality to the Get method in the CacheTestHandlerImplementation. It fetches a value from the cache using a given key, logs any errors, and returns the value or an empty string if the key does not exist. New packages context, log, and net/http have also been imported to support this change.

--- service/handlers/cache_test/CacheTestImplementation.go
+++ service/handlers/cache_test/CacheTestImplementation.go
@@ -1,8 +1,11 @@
 package cache_test
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
+	"log"
+	"net/http"
 )
 
 type CacheTestHandlerImplementation struct {
@@ -10,8 +13,18 @@ type CacheTestHandlerImplementation struct {
 }
 
 func (h *CacheTestHandlerImplementation) Get(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
+	key := c.Param("key")
+	log.Println("Get key:", key)
+
+	result, err := h.cache.Get(context.Background(), key).Result()
+	if err != nil {
+		result = ""
+		log.Println("Get err:", err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"value": result,
+	})
 }
 
 func (h *CacheTestHandlerImplementation) Set(c *gin.Context) {
