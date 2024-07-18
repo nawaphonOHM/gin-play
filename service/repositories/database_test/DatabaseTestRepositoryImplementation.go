46ea4cd9a5860f29a65a853254cc85b8cfcfcca0
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-07-18 11:00:08 +0700

    Add DatabaseTestRepositoryImplementation
    A new file, DatabaseTestRepositoryImplementation.go, has been added under the service/repositories/database_test directory. It contains a skeleton of the implementation for the method GetData() and the file also includes the structure for DatabaseTestRepositoryImplementation with an associated constructor.

---
+++ service/repositories/database_test/DatabaseTestRepositoryImplementation.go
@@ -0,0 +1,16 @@
+package repository_database_test
+
+import "github.com/jmoiron/sqlx"
+
+type DatabaseTestRepositoryImplementation struct {
+	databaseWrapper *sqlx.DB
+}
+
+func (d DatabaseTestRepositoryImplementation) GetData() *Response {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewDatabaseTestRepositoryImplementation(db *sqlx.DB) DatabaseTestRepository {
+	return &DatabaseTestRepositoryImplementation{databaseWrapper: db}
+}
