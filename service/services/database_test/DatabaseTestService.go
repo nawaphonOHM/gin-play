5d1effbbee26431b62ce13dbc183701d1c9bd71e
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-07-18 10:27:20 +0700

    Update GetData method and add Response struct
    The GetData method in DatabaseTestService interface has been updated to return a pointer to a Response struct. Additionally, a new Response struct has been added with Headers and Data fields, both as string slices.

--- service/services/database_test/DatabaseTestService.go
+++ service/services/database_test/DatabaseTestService.go
@@ -1,5 +1,10 @@
 package service_database_test
 
 type DatabaseTestService interface {
-	GetData()
+	GetData() *Response
+}
+
+type Response struct {
+	Headers []string   `json:"headers"`
+	Data    [][]string `json:"data"`
 }
