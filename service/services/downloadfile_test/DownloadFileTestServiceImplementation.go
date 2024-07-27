86488554c93b03f5a7280bcbae0bbaff5892585e
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-07-27 17:25:47 +0700

    Add DownloadFileTestServiceImplementation with basic methods
    Implement the DownloadFileTestServiceImplementation struct along with a GetFile method returning sample content. Additionally, include a constructor for initializing the new service.

--- service/services/downloadfile_test/DownloadFileTestServiceImplementation.go
+++ service/services/downloadfile_test/DownloadFileTestServiceImplementation.go
@@ -1,1 +1,14 @@
 package service_downloadfile_test
+
+type DownloadFileTestServiceImplementation struct {
+}
+
+func (s *DownloadFileTestServiceImplementation) GetFile() (*Response, error) {
+	fileContent := "Hello World. This is a test content"
+
+	return &Response{FileByte: []byte(fileContent)}, nil
+}
+
+func NewDownloadFileTestServiceImplementation() DownloadFileTestService {
+	return &DownloadFileTestServiceImplementation{}
+}
