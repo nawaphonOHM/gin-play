23533e0a453d206df00eacbaae60586afe909384
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-07-27 16:52:58 +0700

    Implement GetData method in DatabaseTestRepository
    The GetData method now executes a query to fetch all data from the "User" table. It processes the rows, scans them, and appends them to the response, logging any errors encountered.

--- service/repositories/database_test/DatabaseTestRepositoryImplementation.go
+++ service/repositories/database_test/DatabaseTestRepositoryImplementation.go
@@ -1,14 +1,34 @@
 package repository_database_test
 
-import "github.com/jmoiron/sqlx"
+import (
+	"github.com/jmoiron/sqlx"
+	"log"
+)
 
 type DatabaseTestRepositoryImplementation struct {
 	databaseWrapper *sqlx.DB
 }
 
 func (r *DatabaseTestRepositoryImplementation) GetData() *Response {
-	//TODO implement me
-	panic("implement me")
+	query := "SELECT * FROM \"User\""
+
+	rows, err := r.databaseWrapper.Query(query)
+	if err != nil {
+		return nil
+	}
+
+	response := new(Response)
+
+	for rows.Next() {
+		row := make([]string, 0)
+		err = rows.Scan(&row)
+		if err != nil {
+			log.Fatal(err)
+		}
+		response.Data = append(response.Data, row)
+	}
+
+	return response
 }
 
 func NewDatabaseTestRepositoryImplementation(db *sqlx.DB) DatabaseTestRepository {
