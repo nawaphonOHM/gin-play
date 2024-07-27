9778df8dfe0090aa35733b9f831cdd86927a071f
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-07-27 17:41:18 +0700

    Add handler and service for file download tests
    This commit introduces a new handler and service for file download tests in the `main.go` file. The routes are set up under the `/download` path, enhancing the project with download functionality.

--- service/main.go
+++ service/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	handler_cache_test "gin-play/handlers/cache_test"
 	handler_database_test "gin-play/handlers/database_test"
+	handler_downloadfile_test "gin-play/handlers/downloadfile_test"
 	handler_hello_world "gin-play/handlers/hello_world"
 	repository_database_test "gin-play/repositories/database_test"
 	service_cache_test "gin-play/services/cache_test"
 	service_database_test "gin-play/services/database_test"
+	service_downloadfile_test "gin-play/services/downloadfile_test"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 	"github.com/redis/go-redis/v9"
@@ -50,6 +52,9 @@ func main() {
 	cacheTestHandler := handler_cache_test.NewCacheTestHandler(cacheTestService)
 	dbTestHandler := handler_database_test.NewDatabaseTestHandlerImplementation(dbTestService)
 
+	downloadFileService := service_downloadfile_test.NewDownloadFileTestServiceImplementation()
+	downloadFileHandler := handler_downloadfile_test.NewDownloadFileTestHandlerImplementation(downloadFileService)
+
 	root := ginDefault.Group("/")
 	{
 		root.GET("/hello-world", helloWorldHandler.MainHandler)
@@ -64,6 +69,11 @@ func main() {
 	dbPath := ginDefault.Group("/db")
 	{
 		dbPath.GET("/hello-world", dbTestHandler.Get)
+	}
+
+	downloadFilePath := ginDefault.Group("/download")
+	{
+		downloadFilePath.GET("/hello-world", downloadFileHandler.Get)
 	}
 
 	if port, isSet := os.LookupEnv("PORT"); isSet {
