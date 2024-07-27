32203bed0ed57ff26ffdbe1bd0a92b7f893ec45d
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2024-07-27 17:39:22 +0700

    Add DownloadFileTestHandler implementation
    Implemented a new handler for downloading test files. This includes the HTTP GET endpoint to serve the file and appropriate error handling. Also added a constructor function for the new handler implementation.

--- service/handlers/downloadfile_test/DownloadFileTestHandlerImplementation.go
+++ service/handlers/downloadfile_test/DownloadFileTestHandlerImplementation.go
@@ -1,1 +1,25 @@
 package handler_downloadfile_test
+
+import (
+	service_downloadfile_test "gin-play/services/downloadfile_test"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type DownloadFileTestHandlerImplementation struct {
+	mainService service_downloadfile_test.DownloadFileTestService
+}
+
+func (h *DownloadFileTestHandlerImplementation) Get(c *gin.Context) {
+	file, err := h.mainService.GetFile()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.Header("Content-Disposition", "attachment; filename=\"file.txt\"")
+	c.Data(http.StatusOK, "application/octet-stream", file.FileByte)
+}
+
+func NewDownloadFileTestHandlerImplementation(mainService service_downloadfile_test.DownloadFileTestService) DownloadFileTestHandler {
+	return &DownloadFileTestHandlerImplementation{mainService: mainService}
+}
